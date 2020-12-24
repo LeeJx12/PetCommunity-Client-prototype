aba6e6a1bfd53d9e5d1048215e8d3e5b79a74084
LeeJx2 <LeeJx2@DESKTOP-ATGAT2G>
2020-12-24 16:04:53 +0900

    PetCommunity Client Prototype

---
+++ pet-comm/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
